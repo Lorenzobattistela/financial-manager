bc99ed32e2919720bb7ced12a53031abaf409e67
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-05-16 08:26:15 -0300

    update routes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 use dotenv::dotenv;
-use rocket::{launch, routes, Rocket, Build};
-use financial_manage::api::{ethereum_balance, bitcoin_balance, upload};
-
+use financial_manage::api::{
+    bitcoin_balance, ethereum_balance, get_btc_brl_price, get_eth_brl_price, upload,
+};
+use rocket::{launch, routes, Build, Rocket};
 
 #[launch]
 pub fn rocket() -> Rocket<Build> {
@@ -15,5 +17,8 @@ pub fn rocket() -> Rocket<Build> {
     rocket::build()
         .mount("/", routes![bitcoin_balance])
         .mount("/", routes![ethereum_balance])
+        .mount("/", routes![get_btc_brl_price])
+        .mount("/", routes![get_eth_brl_price])
         .mount("/", routes![upload])
 }
+
